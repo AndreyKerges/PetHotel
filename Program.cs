cc7c7b10e55546e29193161e7bd59217b5d55fee
AndreyKerges <andrey.nascimento4@etec.sp.gov.br>
2023-04-02 00:01:49 -0300

    exercicio resolvido

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿string especie, raca, alcunha, idade, cor;
+﻿//declara 5 variáveis do tipo string
+string especie, raca, alcunha, idade, cor;
 
+//Escreve no console a string "PET HOTEL"
 Console.WriteLine("PET HOTEL");
 
-
+//Escreve no console a string "Digite a espécie do pet: "
 Console.WriteLine("Digite a espécie do pet: ");
+//Lê a entrada do usuário a partir do console e atribui o valor à variável especie
 especie = Console.ReadLine()!;
 
 Console.WriteLine("Digite a raça do pet: ");
@@ -19,19 +22,34 @@ Console.WriteLine("Digite a cor do pet: ");
 cor = Console.ReadLine()!;
 
 Console.ForegroundColor = ConsoleColor.DarkGray;
+//Escreve uma linha horizontal no console com uma borda superior e inferior formada por
+//símbolos de igual e a cor definida anteriormente
 Console.WriteLine("+=========================================================+");
 
+//Define a cor do texto que será impresso no console como verde
 Console.ForegroundColor = ConsoleColor.Green;
+
+//Escreve no console uma string contendo o nome do hotel e o apelido do pet no centro, 
+//entre duas barras verticais
 Console.WriteLine("|                Pet Hotel - Peteleco                     |");
 
 Console.ForegroundColor = ConsoleColor.DarkGray;
 Console.WriteLine("+=========================================================+");
 
+//Define a cor do texto que será impresso no console como verde
 Console.ForegroundColor = ConsoleColor.Green;
+
+//Escreve no console a string " Espécie:" sem pular uma linha
 Console.Write($"| Espécie:");
+
+//Retorna a cor do texto para a cor padrão do console.
 Console.ResetColor();
 
+//Define a cor do texto que será impresso no console como vermelho
 Console.ForegroundColor = ConsoleColor.Red;
+
+//Escreve no console o valor da variável especie, alinhado à esquerda e preenchido com 
+//pontos para que ocupe 20 caracteres
 Console.Write($"{especie.PadLeft(20, '.')}");
 Console.ResetColor();
 
